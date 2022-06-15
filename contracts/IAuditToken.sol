fb607d9192b28b375e320e5c14e7e9c2f88ba188
win2win <win2win@protonmail.com>
2022-06-15 17:20:11 -0500

    initial upload

---
+++ contracts/IAuditToken.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.0;
+
+    
+interface IAuditToken {
+    function mint(address to, uint256 amount) external returns (bool) ;
+    function transferFrom(address from, address to, uint256 value) external  returns (bool);
+    function transfer(address to, uint256 value) external returns (bool);
+}
