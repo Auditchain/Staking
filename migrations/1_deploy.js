8140b19cfd4da39d180787028614d2adf630fc16
win2win <win2win@protonmail.com>
2022-06-17 09:13:22 -0500

    add grant roles and ganache traveler

--- migrations/1_deploy.js
+++ migrations/1_deploy.js
@@ -7,6 +7,9 @@ var BigNumber = require('big-number');
 let MINTER_ROLE = web3.utils.keccak256("MINTER_ROLE");
 let CONTROLLER_ROLE = web3.utils.keccak256("CONTROLLER_ROLE");
 
+const timeMachine = require('ganache-time-traveler');
+
+
 
 
 module.exports = async function (deployer, network, accounts) { // eslint-disable-line
@@ -19,13 +22,32 @@ module.exports = async function (deployer, network, accounts) { // eslint-disabl
     token = await TOKEN.new(owner);
     let blockTime = await web3.eth.getBlock(blockNumber);
 
-    let endDate = blockTime.timestamp + 2000;
+    let endDate = blockTime.timestamp + (60 );
+    // let endDate = blockTime.timestamp + (60 * 60 );
+
+
 
     staking = await STAKING.new(token.address, endDate);
-    memberHelpers = await MEMBERHELPERS.new();
+    memberHelpers = await MEMBERHELPERS.new(token.address);
+
+    // await token.grantRole(MINTER_ROLE, staking.address, { from: owner });
+    // await token.mint(holder1, tokensToDeposit, { from: owner });
+    // await token.mint(holder2, tokensToDeposit, { from: owner });
+
+
+    await staking.setDepositContract(memberHelpers.address);
+    await memberHelpers.grantRole(CONTROLLER_ROLE, staking.address, { from: owner });
+
     await token.grantRole(MINTER_ROLE, owner, { from: owner });
     await token.grantRole(MINTER_ROLE, staking.address, { from: owner });
-    await token.mint(owner, tokensToDeposit, { from: owner });
+    // await token.mint(owner, tokensToDeposit, { from: owner });
+
+
+    await token.increaseAllowance(staking.address, tokensToDeposit, { from: owner });
+    await staking.stake(tokensToDeposit, { from: owner });
+
+    await timeMachine.advanceTime(60 * 60 * 24 * 366);
+    await timeMachine.advanceBlock();
 
     console.log("\n\n" + '"AUDT_TOKEN_ADDRESS":"' + token.address + '",');
     console.log('"STAKING_CONTRACT_ADDRESS":"' + staking.address + '",');
